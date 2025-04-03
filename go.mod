59269c63be894141e89c70f2334e6cf8fe1d8658
marcusquigley <marcusquigley@gmail.com>
2025-04-03 15:48:54 -0700

    refactor

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/gorilla/mux v1.8.1
+	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.10.0
 )
 
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"todoapp/db"
+	"todoapp/handlers"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+const (
+	sqlHost       = "postgres"
+	sqlConnString = "host=localhost user=postgres password=postgres dbname=todo port=5432 sslmode=disable"
+)
+
 func main() {
+	conn, e := sql.Open(sqlHost, sqlConnString)
+	if e != nil {
+		panic(e)
+	}
+	defer conn.Close()
+
+	repo := db.NewTodoRepo(conn)
+	handler := handlers.NewTodoHandler(repo)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/todos", handler.GetAllTodos)
+	router.HandleFunc("/todos/{id}", handler.GetTodoById)
+	fmt.Println("server is starting on 8000 \n ")
+	if e = http.ListenAndServe(":8000", router); e != nil {
+		panic(e)
+	}
 
 }
